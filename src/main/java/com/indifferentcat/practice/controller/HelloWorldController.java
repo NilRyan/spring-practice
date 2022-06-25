a8d022a54747a6af3761d967240f94d0ce2dcf12
Neil Ryan L <neilryanlipaod@gmail.com>
2022-06-25 22:53:13 +0800

    feat(security): add JwtAuthenticationConverter Bean for parsing roles

--- src/main/java/com/indifferentcat/practice/controller/HelloWorldController.java
+++ src/main/java/com/indifferentcat/practice/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.indifferentcat.practice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +14,13 @@ import java.security.Principal;
 public class HelloWorldController {
 
     @GetMapping
+    @PreAuthorize("hasAuthority('READ')")
     public String helloWorld() {
         return "Hello World!a";
     }
 
     @GetMapping("/test")
+    @PreAuthorize("hasAuthority('READ')")
     public String test(Principal principal) {
         return principal.getName();
     }
--- src/main/java/com/indifferentcat/practice/security/JwtSecurityConfig.java
+++ src/main/java/com/indifferentcat/practice/security/JwtSecurityConfig.java
@@ -7,6 +7,12 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,6 +22,20 @@ public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.oauth2ResourceServer()
                 .jwt();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(jwt ->
+                Optional.ofNullable(jwt.getClaimAsStringList("custom_claims"))
+                        .stream()
+                        .flatMap(Collection::stream)
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList())
+        );
+        return converter;
     }
 
 }
